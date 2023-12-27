c043ffc8a21d70b5c503e779538abe3f35f266d3
Tony Edwards <tony.edwards@gmail.com>
2023-12-27 08:43:13 +0000

    Adds episode selection

---
+++ Models/Episode.cs
@@ -0,0 +1,22 @@
+﻿namespace PodcastPlayer.Models;
+
+public class Episode
+{
+    public string Title 
+    { 
+        get 
+        {
+            string[] pathParts = Path.Split('/');
+            string fileName = pathParts[pathParts.Length - 1];
+            string[] fileNameParts = fileName.Split('-');
+            string episodeTitle = string.Join(" ", fileNameParts);
+            return episodeTitle;
+        }
+    }
+    public string Path { get; set; }
+
+    public Episode(string title, string path)
+    {
+        Path = path;
+    }
+}
--- Views/PodcastPage.xaml.cs
+++ Views/PodcastPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using PodcastPlayer.Models;
+using System.Collections.ObjectModel;
 
 namespace PodcastPlayer.Views;
 
@@ -16,6 +17,18 @@ public partial class PodcastPage : ContentPage
 			OnPropertyChanged(nameof(PageTitle));
 		}
 	}
+
+	private ObservableCollection<Episode> episodes;
+	public ObservableCollection<Episode> Episodes
+	{
+		get { return episodes; }
+		set
+		{
+			episodes = value;
+			OnPropertyChanged(nameof(Episodes));
+		}
+	}
+
 	public Podcast Podcast { get; set; }
 	
 	public PodcastPage()
@@ -29,6 +42,38 @@ public partial class PodcastPage : ContentPage
 		base.OnAppearing();
 		Debug.WriteLine("Podcast Page Appearing");
 		Debug.WriteLine($"Podcast: {Podcast.Title}");
-		PageTitle = $"Welcome to the {Podcast.Title} series page.";
+		PageTitle = $"Welcome to the {Podcast.Title} series page";
+
+		string[] files = getFiles(Podcast.Folder);
+		Episodes = new ObservableCollection<Episode>(
+			files.Select(file => new Episode(file, file))
+		);
+		Debug.WriteLine($"Episodes: {Episodes.Count}");
+	}
+
+	public string[] getFiles(string folder)
+	{
+		Debug.WriteLine("Getting Files");
+		string[] results = new string[1];
+		try
+		{
+			results = Directory.GetFiles(folder);
+			// print files to debug
+			foreach (string file in results)
+			{
+				Debug.Write(file + " - ");
+			}
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine(ex.Message);
+		}
+		return results;
+	}
+
+	public async void Episode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+		var episode = e.CurrentSelection.FirstOrDefault() as Episode;
+		Debug.WriteLine($"Episode Selected: {episode?.Title}");
 	}
 }
